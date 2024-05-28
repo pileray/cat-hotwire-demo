f5278f7e6bc007d3ab83140d4944ef505225f0d0
Ryosuke.Fukui <fukuiryo.0312@gmail.com>
2024-05-28 23:16:20 +0900

    Add: flashのturbo stream化

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -36,6 +36,7 @@ class CatsController < ApplicationController
   def update
     if @cat.update(cat_params)
       # redirect_to @cat, notice: "ねこを更新しました。"
+      flash.now.notice = "ねこを更新しました。"
     else
       render :edit, status: :unprocessable_entity
     end
